976efe6501271da606b57ce27c2b6beb63d4d53e
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-11-25 11:09:29 -0500

    Adding close icon

--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,7 @@ export default function Content(props) {
 
     return (
         <div className={props.contentIsOpen ? "content open" : "content"}>
-            <button className="closeButton" onClick={() => props.openContent(false)}>Close</button>
+            <button className="closeButton material-icons close" onClick={() => props.openContent(false)} tabIndex="0">close</button>
         </div>
     );
 }
