d509ab03f8f052b0df6db9cb66872edc59bd346c
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-11-25 15:56:34 -0500

    Updates

--- precache-manifest.500b7ce95d2e54e30ee4203d0cfe48c8.js
+++ precache-manifest.500b7ce95d2e54e30ee4203d0cfe48c8.js
@@ -1,22 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c467b192c98d47ffedac5f69c59bc2f",
+    "revision": "51e425d78adada8d0bd2a0c3438919e0",
     "url": "/12-Days-of-Data/index.html"
   },
   {
-    "revision": "0d3cf0df883b00492513",
-    "url": "/12-Days-of-Data/static/css/main.cba9a170.chunk.css"
+    "revision": "dc157542bf05964c90e0",
+    "url": "/12-Days-of-Data/static/css/main.3d89b5ee.chunk.css"
   },
   {
     "revision": "cdb1ee3e4d362953102f",
     "url": "/12-Days-of-Data/static/js/2.0acd36b5.chunk.js"
   },
   {
-    "revision": "0d3cf0df883b00492513",
+    "revision": "dc157542bf05964c90e0",
     "url": "/12-Days-of-Data/static/js/main.a0ce78cb.chunk.js"
   },
   {
     "revision": "629a7d6cab75c18dfbf1",
     "url": "/12-Days-of-Data/static/js/runtime-main.629d0bec.js"
+  },
+  {
+    "revision": "f6e8da8e7bfa6c0bf6ae09e63f3f47e0",
+    "url": "/12-Days-of-Data/static/media/bg.f6e8da8e.jpg"
   }
 ]);
